2672df772cdc6499b671bf7a3bc53e2eb4c7cc9f
Skivel <denisbuhrov@gmail.com>
2024-01-31 01:11:22 +0100

    feat!: Rebace from telegram lib to telebot && feat: add search link in user message

---
+++ README.md
@@ -0,0 +1,5 @@
+**How to use:**
+
+- clone repo to your PC
+- in main.py (Line 12) enter your bot token
+- run start.bat (auto install requirements and start Bot)
--- main.py
+++ main.py
@@ -1,77 +1,80 @@
 import os
-from dotenv import load_dotenv, find_dotenv
-
-from telegram import InputMediaPhoto
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
 import requests
+import telebot
+
+from dotenv import load_dotenv, find_dotenv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
 load_dotenv(find_dotenv('.env'))
 
-TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(os.getenv('TOKEN'))
 
 
-def start(update, context):
-    update.message.reply_text("Привіт! Надішліть мені URL OLX, і я поверну вам його вміст.")
+@bot.message_handler(commands=['start'])
+def hello(message):
+    bot.send_message(message.chat.id, f"Привіт, {message.from_user.first_name}! \nНадішліть мені URL оголошення, і я "
+                                      f"поверну вам його фото.")
 
 
-def handle_image_url(update, context, output_folder='downloaded_images'):
-    url = update.message.text
-    print(url)
-    # Send an HTTP request to the provided link
-    response = requests.get(url)
+@bot.message_handler()
+def handle_image_url(message, output_folder='downloaded_images'):
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        update.message.reply_text("Progres: 1/4")
-        # Parse the HTML content of the webpage
-        soup = BeautifulSoup(response.content, 'html.parser')
+    url = False
 
-        # Find all image tags or any other elements that contain image URLs
-        img_tags = soup.find_all('img', {'sizes': True})
+    if message.link_preview_options.url.startswith('https://www.olx'):
+        url = message.link_preview_options.url
 
-        # Create the output folder if it doesn't exist
-        os.makedirs(output_folder, exist_ok=True)
-        update.message.reply_text("Progres: 2/4")
-        # Download each image with the 'sizes' attribute
-        for i, img_tag in enumerate(img_tags):
-            img_url = img_tag['src']
-            img_url = urljoin(url, img_url)  # Make sure the URL is absolute
+    if url:
+        # Send an HTTP request to the provided link
+        response = requests.get(url)
 
-            # Download the image
-            img_response = requests.get(img_url)
-            if img_response.status_code == 200:
-                # Save the image to a file
-                img_filename = f'image_{i + 1}.jpg'
-                img_path = os.path.join(output_folder, img_filename)
-                with open(img_path, 'wb') as img_file:
-                    img_file.write(img_response.content)
-                print(f'Image {i + 1} downloaded successfully.')
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            bot.send_message(message.chat.id, "Progres: 1/4")
+            # Parse the HTML content of the webpage
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Find all image tags or any other elements that contain image URLs
+            img_tags = soup.find_all('img', {'sizes': True})
+
+            # Create the output folder if it doesn't exist
+            os.makedirs(output_folder, exist_ok=True)
+            bot.send_message(message.chat.id, "Progres: 2/4")
+            # Download each image with the 'sizes' attribute
+            for i, img_tag in enumerate(img_tags):
+                img_url = img_tag['src']
+                img_url = urljoin(url, img_url)  # Make sure the URL is absolute
+
+                # Download the image
+                img_response = requests.get(img_url)
+                if img_response.status_code == 200:
+                    # Save the image to a file
+                    img_filename = f'image_{i + 1}.jpg'
+                    img_path = os.path.join(output_folder, img_filename)
+                    with open(img_path, 'wb') as img_file:
+                        img_file.write(img_response.content)
+                    print(f'Image {i + 1} downloaded successfully.')
+                else:
+                    bot.send_message(message.chat.id,
+                                     f'Failed to download image {i + 1}. Status code: {img_response.status_code}')
+            bot.send_message(message.chat.id, "Progres: 3/4")
+
+            media_group = []
+
+            for i, img_tag in enumerate(img_tags):
+                photo_path = f'downloaded_images/image_{i + 1}.jpg'
+                media_group.append(telebot.types.InputMediaPhoto(media=open(photo_path, 'rb')))
+            bot.send_message(message.chat.id, "Progres: 4/4")
+            if media_group:
+                bot.send_media_group(message.chat.id, media_group)
             else:
-                update.message.reply_text(f'Failed to download image {i + 1}. Status code: {img_response.status_code}')
-        update.message.reply_text("Progres: 3/4")
-        media_group = []
-
-        for i, img_tag in enumerate(img_tags):
-            photo_path = f'downloaded_images/image_{i + 1}.jpg'
-            media_group.append(InputMediaPhoto(media=open(photo_path, 'rb')))
-        update.message.reply_text("Progres: 4/4")
-        if media_group:
-            update.message.reply_media_group(media=media_group, timeout=60)
+                bot.send_message(message.chat.id, "No images to send.")
         else:
-            update.message.reply_text("No images to send.")
+            bot.send_message(message.chat.id, f'Failed to retrieve webpage. Status code: {response.status_code}')
     else:
-        update.message.reply_text(f'Failed to retrieve webpage. Status code: {response.status_code}')
+        bot.send_message(message.chat.id, 'У вашому повідомлені немає посилань на OLX')
 
-if __name__ == '__main__':
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_image_url))
-
-    updater.start_polling()
-    updater.idle()
+bot.polling(none_stop=True)
